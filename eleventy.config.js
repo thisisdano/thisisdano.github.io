e534db1e3d9227e257809283cc06de8c2412abc3
Dan O. Williams <11464021+thisisdano@users.noreply.github.com>
2025-04-23 12:37:23 -0700

    Passthrough splide files

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,4 +9,10 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/css");
   eleventyConfig.addPassthroughCopy("projects/berkman/img");
   eleventyConfig.addPassthroughCopy("browserconfig.xml");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@splidejs/splide/dist/js": "js/splide",
+  });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@splidejs/splide/dist/css": "styles/splide",
+  });
 }
