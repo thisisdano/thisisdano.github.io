4a9b28e60f86d926d44481144a839b14bd3cd850
Dan O. Williams <11464021+thisisdano@users.noreply.github.com>
2025-04-24 10:45:58 -0700

    Initialize all sliders

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,9 +3,9 @@ import Splide from "/js/splide/splide.esm.js";
 console.log("Oh hi!");
 
 const options = {
-  type: "fade",
+  pagination: false,
   rewind: true,
-  arrows: false,
+  type: "fade",
 };
 
 const advance = (instance, current) => {
@@ -19,3 +19,23 @@ const advance = (instance, current) => {
 const lil = new Splide("#library-innovation-lab .splide", options);
 lil.mount();
 lil.on("click", (slide) => advance(lil, slide));
+
+const perma = new Splide("#perma-cc .splide", options);
+perma.mount();
+perma.on("click", (slide) => advance(perma, slide));
+
+const ftl = new Splide("#free-the-law .splide", options);
+ftl.mount();
+ftl.on("click", (slide) => advance(ftl, slide));
+
+const berk = new Splide("#berkman-center .splide", options);
+berk.mount();
+berk.on("click", (slide) => advance(berk, slide));
+
+const npoWeb = new Splide("#n-plus-one-website .splide", options);
+npoWeb.mount();
+npoWeb.on("click", (slide) => advance(npoWeb, slide));
+
+const npoMag = new Splide("#n-plus-one-magazine .splide", options);
+npoMag.mount();
+npoMag.on("click", (slide) => advance(npoMag, slide));
